45a3cfc0b206db16d71cf5fea93975dc7db94b3a
ZiggyBaz <basil99magawi@gmail.com>
2024-07-15 16:41:54 +0300

    Finalizing the first step: initializing the .git directory

--- git/src/main.rs
+++ git/src/main.rs
@@ -1,3 +1,15 @@
+use std::{fs, env};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args[1] == "init" {
+        fs::create_dir(".git").unwrap();
+        fs::create_dir(".git/objects").unwrap();
+        fs::create_dir(".git/refs").unwrap();
+        fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+
+        println!("Initialized git directory");
+    } else {
+        println!("unknown command: {}", args[1]);
+    }
 }
